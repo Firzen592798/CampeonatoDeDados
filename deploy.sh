f963fa9f6751fd048d78cd30cd07a99086d92d2a
Augusto <augusto@imd.ufrn.br>
2020-08-25 19:25:19 -0300

    deploy

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,7 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f git@github.com:Firzen592798/Firzen592798.github.io.git master:gh-pages
 
 cd -
+
